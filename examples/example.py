4c41a079a0bd7bfed75b4a9a5b34f4bab532f05a
Vyshnavmt94 <vyshnav94.mec@gmail.com>
2021-09-07 22:30:02 +0530

    Added new styles

--- examples/example.py
+++ examples/example.py
@@ -3,11 +3,8 @@ import os
 from style_transfer.train import Trainer
 from style_transfer.utils import load_img
 
-# content_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), "style_transfer/content/content_1.png")
-# style_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), "style_transfer/style/style_1.png")
-
-content_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), "style_transfer/content/deadpool.jpg")
-style_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), "style_transfer/style/guernica.jpg")
+content_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), "style_transfer/content/content_1.png")
+style_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), "style_transfer/style/style_1.png")
 
 if __name__ == "__main__":
     content_image = load_img(content_path)
